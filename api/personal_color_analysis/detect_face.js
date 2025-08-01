d23a13d8e8ccb54e336276764697aeb60a3fa86f
JkhTV <candzzang5283@gmail.com>
2025-08-01 16:17:46 +0900

    Feat tiny shape predictor 68 face landmarks

--- api/personal_color_analysis/detect_face.js
+++ api/personal_color_analysis/detect_face.js
@@ -3,19 +3,28 @@ const { loadImage } = require("canvas");
 const path = require("path");
 
 // 모델 파일 경로
-const MODEL_URL = path.join(__dirname, "models", "shape_predictor_68_face_landmarks.dat");
+const MODEL_URL = path.join(__dirname, "models");
 
 // 모델 로딩 (최초 한 번만 호출)
 async function loadModels() {
+    console.log(`Loading face-api.js models from: ${MODEL_URL}`);
     await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
+    await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(MODEL_URL);
     console.log("Face API models loaded successfully.");
 }
 
 async function detectFaceAndLandmarks(imageBuffer) {
     try {
         const image = await loadImage(imageBuffer);
-        const detections = await faceapi.detectSingleFace(image).withFaceLandmarks();
+
+        // 경량화 모델을 사용
+        const options = new faceapi.SsdMobilenetv1Options();
+        const detections = await faceapi.detectSingleFace(image, options).withFaceLandmarks(true);
+
+        if (!detections) {
+             return null;
+        }
+        
         return { image, detections };
     } catch (error) {
         console.error("Error detecting face and landmarks:", error);
